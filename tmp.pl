0c1049bae6549828b9fbda8ad65ee531518f7e9f
U-NUANCE\dan_wells <dan_wells@DE-EM00108.nuance.com>
2017-01-16 17:37:45 +0000

    more

--- tmp.pl
+++ tmp.pl
@@ -1,90 +1,46 @@
 #!/usr/local/bin/perl
 use utf8;
 use strict;
-use v5.10;
 
-sub sum_of_a_and_b {
-    print "you summed a and b!\n";
-    $a + $b;
-}
-
-$a = 2;
-$b = 3;
-
-my $c = &sum_of_a_and_b;
-print "and the answer is $c!\n";
+# reading from <STDIN> in scalar context takes the next line only
+# or only accepts one line from terminal, no ctrl-d required
+# print "Give us a line: ";
+# my $line;
+# chomp($line = <STDIN>);
+# print "You said \"$line\"!\n";
 
 
-sub larger_of_a_or_b {
-    if ($a > $b) {
-        $a;
-    } else {
-        $b;
-    }
-}
-
-my $d = &larger_of_a_or_b;
-print "$d\n";
-
-
-# by default all variables in perl are global
-# private variables (scoped within a block) can be declared using my
-# nb. `my $a, $b = @_;` is scalar context and will ONLY declare $a
-#     `my($a, $b) = @_;` is array context and will declare both
-
-# sub max {
-#     # if (@_ != 2) {
-#     #     print "WARNING! &max should get exactly two arguments!\n";
-#     # }
-#     # my($m, $n);
-#     # ($m, $n) = @_; # @_ contains the list of sub arguments
-#     # or just do this in one line
-#     my($m, $n) = @_;
-#     if ($m > $n) { $m } else { $n };
+# to read all lines until the end of a file, just do
+# while (<STDIN>) {
+# 	print "I saw $_";
 # }
 
-sub max {
-    my $max_so_far = shift @_; # set first input value to current max
-    foreach (@_) {
-        if ($_ > $max_so_far) {
-            $max_so_far = $_;
-        }
-    }
-    $max_so_far;
-}
 
-my @numbers = (3, 5, 10, 4, 6);
-my $big = max(@numbers);
-print "The biggest is $big\n";
+# evaluating line input in a list context loads all lines into an array before executing following block
+# -> so can be expensive
+# this will wait for a ctrl-d before printing anything
+# foreach (<STDIN>) {
+# 	print "I saw $_";
+# }
 
 
-# the return operator returns a value from a sub immediately
-my @names = qw/ fred barney betty dino wilma pebbles bamm-bamm /;
-my $result = &which_element_is("dino", @names);
-print "dino is number $result\n";
+# the diamond operator <> will read input serially from command line options
+# e.g. `./my_script one two three` reads input from files one two three as if they were concatenated
+# can also read from stdin (`-` on command line) or a pipe
+# while (<>) {
+# 	chomp; # chomp works on $_ with no other arguments
+# 	print "It was $_ in file $ARGV that I saw!\n"; # $ARGV contains current argument (=filename here)
+# }
 
-sub which_element_is {
-    my($what, @array) = @_; 
-    foreach (0..$#array) {
-        if ($what eq $array[$_]) {
-            return $_;
-        }
-    }
-    -1; # return this if no match found
-}
+# could parse contents of @ARGV to look for e.g. args beginning - => option flags
+# good for simple things but otherwise should use Getopt module
+
+# can also populate @ARGV explicitly and read from that using <>
+@ARGV = qw( file1.txt file2.txt file3.txt );
 
 
-# the value of private variables in subs can be preserved between invocations using state
-# nb. needs `use v5.10` to use
-sub running_sum {
-    state $sum = 0;
-    state @numbers;
-    foreach my $number ( @_ ) {
-        push @numbers, $number;
-        $sum += $number;
-    }
-    print "The sum of (@numbers) is $sum\n";
-}
-&running_sum( 5, 6 );
-&running_sum( 1..3 );
-&running_sum( 4 );
+# nb. difference between printing and interpolating arrays
+print @ARGV; # concatenates items with no spaces
+print "@ARGV"; # interpolated result inserts spaces between items
+
+# p.89 formatted output with printf
